afafdfc5ff00414db8bed449512da54b97860fef
Eagan Martin <mece.martinece@gmail.com>
2018-10-29 14:44:51 +0800

    Initial commit;

---
+++ EJS.js
@@ -0,0 +1,18 @@
+/**
+ * This is an opensource project that aims to provide useful wrappers for 
+ * faster development when using ExtJS library.
+ *
+ * Feel free to add more wrappers under categories provided or make another one
+ * if the functionality requires.
+ */
+
+
+/**
+ * A namespace EJS would be used for this project
+ * @type {Object}
+ */
+var EJS = {
+	
+	
+
+}
